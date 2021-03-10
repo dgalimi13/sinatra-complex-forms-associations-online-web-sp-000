77d84e37f97ebf90a73f6808cc192f34b9a26749
danny galimi <dannygalimi13@aim.com>
2021-03-10 21:02:00 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -9,17 +9,16 @@ class PetsController < ApplicationController
     erb :'/pets/new'
   end
 
-  post '/pets' do
+   post '/pets' do
     @pet = Pet.create(params[:pet])
 
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
     end
-  end
 
-  get '/pets/:id' do 
-    @pet = Pet.find(params[:id])
-    erb :'/pets/show'
+    @pet.save
+
+    redirect to "pets/#{@pet.id}"
   end
 
   patch '/pets/:id' do 
